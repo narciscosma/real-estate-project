d1ba2cc7c95a039f0007877d85ad767d6db069df
Narcis Cosma <narcis@graffino.com>
2022-03-14 17:10:01 +0200

    started working on header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <Header />
   </div>
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="header__container">
+        <div class="header__wrapper">
+          <nav class="header__navigation">
+            <h1>l'estate</h1>
+            <div class="header__links">
+              <router-link to="/">Home</router-link> |
+              <router-link to="/about">About</router-link>
+              <router-link to="/houses">Houses</router-link>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import "../assets/scss/modules/header.scss";
+
+export default {
+  name: "HeaderPrimary",
+};
+</script>
+
+<style lang="scss">
+nav {
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #22c2af;
+    }
+  }
+}
+</style>
